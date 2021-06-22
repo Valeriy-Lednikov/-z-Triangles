4f9ce9d0f0c4b50432388a9b471bf32c71e2316c
Valerii-Lednikov <60419585+Valeriy-Lednikov@users.noreply.github.com>
2021-06-22 14:36:58 +0300

    Last

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -18,15 +18,18 @@ namespace WindowsFormsApplication1
         String filePath;
         String[] FileContent;//Файл построчно
 
+        int Max_x = 0;//Максимальные координаты по X,Y
+        int Max_y = 0;
+
         public Triangle[] Triangles;//Массив треугольников
         public struct Triangle//Структура треугольника
         {
-            public Single x1;
-            public Single y1;
-            public Single x2;
-            public Single y2;
-            public Single x3;
-            public Single y3;
+            public int x1;
+            public int y1;
+            public int x2;
+            public int y2;
+            public int x3;
+            public int y3;
         }
 
         public Form1()
@@ -36,6 +39,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
+            Max();
             Draw();//Вызов отрисовки
 
             //g.DrawLines(new Pen(Color.Red, 3), new[] { point1, point2, point3, point1 } );
@@ -58,34 +62,69 @@ namespace WindowsFormsApplication1
             int FileLength = FileContent.Length;//Количество строк в файле
             for (int i = 0; i < TriangleCount; i++)//Проходимся по всем строкам и вытаскиваем треугольники
             {
-                String[] Data = FileContent[i+1].Split(' ');//Разбиваем строку на данные
+                String[] Data = FileContent[i + 1].Split(' ');//Разбиваем строку на данные
                 //Создаём треугольник
-                Triangles[i].x1 = Single.Parse(Data[0]);
-                Triangles[i].y1 = Single.Parse(Data[1]);
-                Triangles[i].x2 = Single.Parse(Data[2]);
-                Triangles[i].y2 = Single.Parse(Data[3]);
-                Triangles[i].x3 = Single.Parse(Data[4]);
-                Triangles[i].y3 = Single.Parse(Data[5]);
+                Triangles[i].x1 = Convert.ToInt32(Data[0]);
+                Triangles[i].y1 = Convert.ToInt32(Data[1]);
+                Triangles[i].x2 = Convert.ToInt32(Data[2]);
+                Triangles[i].y2 = Convert.ToInt32(Data[3]);
+                Triangles[i].x3 = Convert.ToInt32(Data[4]);
+                Triangles[i].y3 = Convert.ToInt32(Data[5]);
             }
 
         }
 
         void Draw()
         {
+            Bitmap TrianglesImage = new Bitmap(Max_x, Max_y);
+            Graphics flagGraphics = Graphics.FromImage(TrianglesImage);
+
             var _Pen = new Pen(Color.Red, 1);
             var g = pictureBox1.CreateGraphics();
 
 
             for (int i = 0; i < TriangleCount; i++)//Проходимся по всем треугольникам
             {
-                var Point1 = new PointF(Triangles[i].x1, Triangles[i].y1);//Задаём координаты 3х точек
-                var Point2 = new PointF(Triangles[i].x2, Triangles[i].y2);
-                var Point3 = new PointF(Triangles[i].x3, Triangles[i].y3);
-                g.DrawLines((_Pen), new[] { Point1, Point2 });//рисуем
-                g.DrawLines((_Pen), new[] { Point1, Point3 });
-                g.DrawLines((_Pen), new[] { Point2, Point3 });
+                var Point1 = new Point(Triangles[i].x1, Triangles[i].y1);//Задаём координаты 3х точек
+                var Point2 = new Point(Triangles[i].x2, Triangles[i].y2);
+                var Point3 = new Point(Triangles[i].x3, Triangles[i].y3);
+
+                DrawLineInt(TrianglesImage, Point1, Point2);//Рисуем
+                DrawLineInt(TrianglesImage, Point1, Point3);
+                DrawLineInt(TrianglesImage, Point2, Point3);
+
+
+            }
+            pictureBox1.Image = TrianglesImage;
+
+        }
+
+        void Max()
+        {
+            for (int i = 0; i < TriangleCount; i++)//Проходимся по всем строкам и вытаскиваем треугольники
+            {
+                String[] Data = FileContent[i + 1].Split(' ');//Разбиваем строку на данные
+                //Ищем максимальный X
+                if (Convert.ToInt32(Data[0]) > Max_x) Max_x = Convert.ToInt32(Data[0]);
+                if (Convert.ToInt32(Data[2]) > Max_x) Max_x = Convert.ToInt32(Data[2]);
+                if (Convert.ToInt32(Data[4]) > Max_x) Max_x = Convert.ToInt32(Data[4]);
+                //Ищем максимальный Y
+                if (Convert.ToInt32(Data[1]) > Max_y) Max_y = Convert.ToInt32(Data[1]);
+                if (Convert.ToInt32(Data[3]) > Max_y) Max_y = Convert.ToInt32(Data[3]);
+                if (Convert.ToInt32(Data[5]) > Max_y) Max_y = Convert.ToInt32(Data[5]);
             }
 
+        }
+
+
+        public void DrawLineInt(Bitmap bmp, Point a, Point b)//Функция отрисовки линии
+        {
+            Pen blackPen = new Pen(Color.Black, 1);
+
+            using (var graphics = Graphics.FromImage(bmp))
+            {
+                graphics.DrawLine(blackPen, a.X, a.Y, b.X, b.Y);
+            }
         }
 
 
